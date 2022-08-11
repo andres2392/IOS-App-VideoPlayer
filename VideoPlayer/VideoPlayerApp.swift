047a35dd08696e56123860990a2b84f3d9e60e25
Andres <andres.gonzalez2392@gmail.com>
2022-08-11 15:12:39 -0400

    Initial Commit

---
+++ VideoPlayer/VideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerApp.swift
+//  VideoPlayer
+//
+//  Created by PDWS on 8/11/22.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
